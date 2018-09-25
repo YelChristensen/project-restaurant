a56822be058e3d3711aa8cadd10f122eb8e11836
YelChristensen <yelena.christensen@gmail.com>
2018-09-25 14:16:53 +0100

    started on CSS

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import Menu from './Menu';
-// import Locations from './Locations';
+import Locations from './Locations';
 
-const menuItems = [{id: 1, name:"Cheeseburger", price: 5}, {id:2, name:"fries", price: 2}, {id: 3, name:"pizza", price: 5}]
+const menuItems = [{id: 1, name:"Cheeseburger", price: 5, img:"./cheesebuger.jpg"}, {id:2, name:"Fries", price: 2}, {id: 3, name:"Pizza", price: 5},{id:4, name:"Salad", price: 3}, {id:5, name:"Chicken", price:6}]
 const locations = [{id:1, streetAddress:"1 Magnolia Street", city:"London"}, {id:2, streetAddress:"177 Maple Road", city: "Bristol"}, {id:3, streetAddress:"1 Clover Lane", city:"Aberdeen"}]
 
 class App extends React.Component {
   render(){
     return (
-      <div>
+      <div className="container">
         <div className="menu">
             <Menu menuItems={menuItems} />
         </div>
        <div className="locations">
-            <ul>
-              {locations.map(location =>
-      <ul key={location.id}><li>{location.streetAddress}</li>
-      <li>{location.city}</li>
-      </ul>
-              )}
-            </ul>
+            <Locations locations={locations} />
        </div>
       </div>
     )
